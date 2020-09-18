4f6a25429e6151effa609f6e3e98db7fad57daa2
Levy Cristian <jolevy.cristian@gmail.com>
2020-09-18 00:26:01 -0300

    Trail cell created

--- lib/View/Components/CustomTabBar.dart
+++ lib/View/Components/CustomTabBar.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:zootracker/View/Trilhas/TrilhasView.dart';
+import 'package:zootracker/View/Trails/TrailsView.dart';
 import 'package:zootracker/main.dart';
 
 class CustomTabBar extends StatefulWidget {
-  CustomTabBar({Key key}) : super(key: key);
+   CustomTabBar({Key key}) : super(key: key);
 
   @override
   CustomTabBarState createState() => CustomTabBarState();
@@ -27,27 +27,27 @@ class CustomTabBarState extends State<CustomTabBar> {
           if (index == 0) {
             return CupertinoTabView(
               navigatorKey: firstTabNavKey,
-              builder: (BuildContext context) => TrilhasView(),
+              builder: (BuildContext context) => TrailsView(),
             );
           } else if (index == 1) {
             return CupertinoTabView(
               navigatorKey: secondTabNavKey,
-              builder: (BuildContext context) => TrilhasView(),
+              builder: (BuildContext context) => TrailsView(),
             );
           } else if (index == 2) {
             return CupertinoTabView(
               navigatorKey: thirdTabNavKey,
-              builder: (BuildContext context) => TrilhasView(),
+              builder: (BuildContext context) => TrailsView(),
             );
           } else if (index == 3) {
             return CupertinoTabView(
               navigatorKey: fourthTabNavKey,
-              builder: (BuildContext context) => TrilhasView(),
+              builder: (BuildContext context) => TrailsView(),
             );
           } else {
             return CupertinoTabView(
               navigatorKey: fifthTabNavKey,
-              builder: (BuildContext context) => TrilhasView(),
+              builder: (BuildContext context) => TrailsView(),
             );
           }
         });
---
+++ lib/View/Trails/TrailsView.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:zootracker/Model/Trail.dart';
+import 'package:zootracker/View/Components/CustomNavBar.dart';
+import 'package:zootracker/View/Trails/TrailsCell.dart';
+
+class TrailsView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return CustomNavBar(
+        text: "Trilhas", uniqueHeroTag: 'tab1', body: TrailsContainerView());
+  }
+}
+
+class TrailsContainerView extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: ListView.separated(
+          separatorBuilder: (context, index) => Divider(
+            color: Colors.grey
+          ),
+          itemCount: mockTrails.length,
+          itemBuilder: (BuildContext context, int index) {
+            return Padding(
+              padding: const EdgeInsets.only(top: 16, left: 16, right: 16, bottom: 0),
+              child: TrailCell(
+                trail: mockTrails[index],
+              ),
+            );
+          },
+        ),
+      ),
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.only(bottom: 50.0),
+        child: FloatingActionButton(
+          tooltip: 'Increment Counter',
+          child: const Icon(Icons.add),
+        ),
+      ),
+    );
+  }
+}
