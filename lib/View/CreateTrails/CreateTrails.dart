fbaa0ed965ac738e751c7b54f9e943c2d8b817ff
Levy Cristian <jolevy.cristian@gmail.com>
2020-09-21 19:22:16 -0300

    Custom image picker implemented

--- lib/View/CreateTrails/CreateTrails.dart
+++ lib/View/CreateTrails/CreateTrails.dart
@@ -1,22 +1,30 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:zootracker/View/Components/CustomNavBar.dart';
 import 'package:zootracker/View/Components/DateInputField.dart';
+import 'package:zootracker/View/Components/ImagePicker.dart';
 import 'package:zootracker/View/Components/TextInputField.dart';
 
 class CreateTrails extends StatefulWidget {
   DateTime selectedDate = DateTime.now();
   TextEditingController dateController = TextEditingController();
-
+  File trailBanner;
 
   @override
-  _CreateTrailsState createState() => _CreateTrailsState(selectedDate: this.selectedDate, dateController: this.dateController);
+  _CreateTrailsState createState() => _CreateTrailsState(
+      selectedDate: this.selectedDate,
+      dateController: this.dateController,
+      trailBanner: this.trailBanner);
 }
 
 class _CreateTrailsState extends State<CreateTrails> {
   DateTime selectedDate;
   TextEditingController dateController;
+  File trailBanner;
 
-  _CreateTrailsState({this.selectedDate, this.dateController});
+  _CreateTrailsState(
+      {this.selectedDate, this.dateController, this.trailBanner});
 
   @override
   Widget build(BuildContext context) {
@@ -29,6 +37,9 @@ class _CreateTrailsState extends State<CreateTrails> {
               padding: const EdgeInsets.only(top: 16),
               child: ListView(
                 children: <Widget>[
+                  CustomImagePicker(
+                    image: trailBanner,
+                  ),
                   TextInputField(
                     title: "Nome da Trilha:",
                   ),
