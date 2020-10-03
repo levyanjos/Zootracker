ad0449c4c4cdd2e1906e82620ffa1ea257c0e61d
Levy Cristian <jolevy.cristian@gmail.com>
2020-10-03 19:03:55 -0300

    Update Trail.dart
    creating new viewmodel to trails

--- lib/Model/Trail.dart
+++ lib/Model/Trail.dart
@@ -1,14 +1,47 @@
+import 'package:flutter/foundation.dart' as foundation;
 
 class Trail {
-  String bannerImage;
-  String title;
-  DateTime date;
+  final String bannerImage;
+  final String title;
+  final DateTime date;
 
-  Trail({this.bannerImage, this.title, this.date});
+  const Trail({this.bannerImage, this.title, this.date});
 }
 
-var mockTrails = <Trail>[
-  Trail(bannerImage: "", title: "Forest - 1", date: DateTime.now()),
-  Trail(bannerImage: "", title: "Forest - 2", date: DateTime.now()),
-  Trail(bannerImage: "", title: "Forest - 3", date: DateTime.now())
-];
+class TrailRepository {
+  static var mockTrails = <Trail>[
+    Trail(bannerImage: "", title: "Forest - 1", date: DateTime.now()),
+    Trail(bannerImage: "", title: "Forest - 2", date: DateTime.now()),
+    Trail(bannerImage: "", title: "Forest - 3", date: DateTime.now())
+  ];
+}
+
+class TrailsViewModel extends foundation.ChangeNotifier {
+  List<TrailCellViewModel> _trailCellsViewModel;
+
+  void loadTrails() {
+    _trailCellsViewModel = TrailRepository.mockTrails.map((e) => TrailCellViewModel(trail: e)).toList();
+    notifyListeners();
+  }
+
+  List<TrailCellViewModel> getTrailCellsViewModel() {
+    if (_trailCellsViewModel == null) {
+      return [];
+    } else {
+      return _trailCellsViewModel;
+    }
+  }
+
+  List<TrailCellViewModel> search(String searchTerms) {
+    return getTrailCellsViewModel().where((product) {
+      return product.trail.title.toLowerCase().contains(searchTerms.toLowerCase());
+    }).toList();
+  }
+}
+
+class TrailCellViewModel extends foundation.ChangeNotifier {
+
+  final Trail trail;
+  TrailCellViewModel({this.trail}) : assert(trail != null);
+
+}
