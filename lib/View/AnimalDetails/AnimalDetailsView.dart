4c0f7cad359a1ae1152d87ecfd68074721066bd8
Levy Cristian <jolevy.cristian@gmail.com>
2020-10-05 15:20:14 -0300

    details flat implemented

--- lib/View/AnimalDetails/AnimalDetailsView.dart
+++ lib/View/AnimalDetails/AnimalDetailsView.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:zootracker/Model/Trail.dart';
 import 'package:zootracker/View/Components/Bars/CustomNavBar.dart';
 
@@ -17,7 +18,53 @@ class _AnimalDetailsViewState extends State<AnimalDetailsView> {
     return CustomNavBar(
       title: Text(widget.trilha.title),
       uniqueHeroTag: "AnimalDetailsViewNavBar",
-      body: Container(),
+      body: MaterialApp(
+        home: DefaultTabController(
+          length: 3,
+          child: Scaffold(
+            appBar: AppBar(
+              bottom: TabBar(
+                tabs: [
+                  Tab(icon: Icon(Icons.directions_car)),
+                  Tab(icon: Icon(Icons.location_on)),
+                  Tab(icon: Icon(Icons.directions_bike)),
+                ],
+              ),
+            ),
+            body: TabBarView(
+              children: [
+                new Container(
+                  color: Colors.deepOrangeAccent,
+                  child: new Center(
+                    child: new Text(
+                      "Primeira Guia",
+                      style: TextStyle(),
+                    ),
+                  ),
+                ),
+                new Container(
+                  color: Colors.blueGrey,
+                  child: new Center(
+                    child: new Text(
+                      "Segunda guia",
+                      style: TextStyle(),
+                    ),
+                  ),
+                ),
+                new Container(
+                  color: Colors.teal,
+                  child: new Center(
+                    child: new Text(
+                      "Terceira guia",
+                      style: TextStyle(),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
